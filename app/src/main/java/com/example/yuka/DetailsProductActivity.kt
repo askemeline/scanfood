cdd11e261d5f7553d60c249185393c6ddbbabf71
Emeline <emeline.garo@supinternet.fr>
2019-07-02 18:17:31 +0200

    update fragment

--- app/src/main/java/com/example/yuka/DetailsProductActivity.kt
+++ app/src/main/java/com/example/yuka/DetailsProductActivity.kt
@@ -1,16 +1,19 @@
 package com.example.yuka
 
+import android.graphics.Typeface
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.ActionBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import kotlinx.android.synthetic.main.list_item_cell.view.*
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.product_detail.*
 import kotlinx.android.synthetic.main.product_detail.view.*
 
 class DetailsProductActivity : AppCompatActivity() {
@@ -29,7 +32,7 @@ class DetailsProductActivity : AppCompatActivity() {
         // myTextView.text = product.name
 
 
-        setContentView(R.layout.fragment_first)
+        setContentView(R.layout.detail_fragment)
         supportFragmentManager.beginTransaction()
             .replace(R.id.detail_with_fragment, ProductFragment.newInstance(product)).commit()
     }
@@ -41,12 +44,11 @@ class DetailsProductActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
 
-
     }
 }
 
 class ProductFragment : Fragment() {
-//la maniere de donner à un Fragment des arguments
+    //la maniere de donner à un Fragment des arguments
     companion object {
         fun newInstance(product: Product): ProductFragment {
             val fragment = ProductFragment()
@@ -66,11 +68,60 @@ class ProductFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
 //        on recupere le produit envoyé au Fragment
-        val product = arguments?.getParcelable< Product >("product")
+        val product = arguments?.getParcelable<Product>("product")
 
 //        on bind le product à la vue
 //        ex: product_name.text= product.name
         view.description_product.text = product?.name
+        view.mark.text = product?.mark
+        title_ingredient.text = product?.ingredient?.joinToString(separator = ", ")
+        title_ingredient.setTitleValue("Ingredients : ", formatList(product!!.ingredient))
+        description_product.text = product.name
+        mark.text = product.mark
+        title_bar_code.text = product.barCode
+
+
+        title_bar_code.setTitleValue("Code barres : ", product.barCode)
+        title_quantity.setTitleValue("Quantité:", product.quantity)
+
+
+        title_from.text = product.countryFrom.joinToString(separator = ", ")
+        title_from.setTitleValue("Vendu en : ", formatList(product.countryFrom))
+
+        title_allergen.text = product.allergen.joinToString(separator = ", ")
+        title_allergen.setTitleValue("Substances allergènes : ", formatList(product.allergen))
+
+        title_addictifs.text = product.addictif.joinToString(separator = ", ")
+        title_addictifs.setTitleValue("Addictifs : ", formatList(product.addictif))
+
+
+        val nutriscore = "B"
+
+        nutriscore_img.setImageResource(
+            resources.getIdentifier(
+                "nutriscore${nutriscore.toLowerCase()}",
+                "drawable",
+                context?.getPackageName()
+            )
+        )
+
+        Picasso.get().load(product.imgUrl).into(url_image)
+
     }
 
+    fun formatList(list: List<String>): String {
+        return if (list.isEmpty()) {
+            getString(R.string.empty_ingredient)
+        } else {
+            list.joinToString(separator = ",")
+        }
+    }
+
+
+}
+
+fun TextView.setTitleValue(title: String, value: String) {
+    text = SpannableString("$title $value").apply {
+        setSpan(StyleSpan(Typeface.BOLD), 0, title.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+    }
 }
--- app/src/main/java/com/example/yuka/MainActivity.kt
+++ app/src/main/java/com/example/yuka/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.example.yuka
 
-import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.style.StyleSpan
 import android.view.*
-import android.widget.TextView
-import android.widget.Toast
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.squareup.picasso.Picasso
 import kotlinx.android.parcel.Parcelize
 import kotlinx.android.synthetic.main.list.*
-import kotlinx.android.synthetic.main.product_detail.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,12 +15,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.list)
 
-
         supportActionBar?.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.toolbar))
-        supportActionBar?.title = getString(R.string.title_toolbar)
+        supportActionBar?.title = getString(R.string.products)
+//        Toast.makeText(this, "Hello World!", Toast.LENGTH_LONG).show();
 
         list.layoutManager = LinearLayoutManager(this)
-
 
         val products = listOf(
             Product(
@@ -95,24 +85,6 @@ class MainActivity : AppCompatActivity() {
         )
         list.adapter = ProductAdapter(products)
 
-        val product = Product(
-            "Petits pois et carottes",
-            "Cassegrain",
-            "3083680085304",
-            "A",
-            "https://static.openfoodfacts.org/images/products/308/368/008/5304/front_fr.7.400.jpg",
-            "400 g (280 g net égoutté)",
-            listOf("France", "Japon", "Suisse"),
-            listOf("Petits pois 66%", "eau", "garniture 2,8% (salade, oignon grelot)", "sucre", "sel", "arôme naturel"),
-            listOf("Aucun"),
-            listOf("Aucune"),
-            12
-        )
-
-        Toast.makeText(this, "Hello World!", Toast.LENGTH_LONG).show();
-//        setContentView(R.layout.list_item_cell)
-
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -129,55 +101,6 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    fun update(product: Product) {
-        description_product.text = product.name
-        mark.text = product.mark
-        title_bar_code.text = product.barCode
-
-
-        title_bar_code.setTitleValue("Code barres : ", product.barCode)
-        title_quantity.setTitleValue("Quantité:", product.quantity)
-
-        title_ingredient.text = product.ingredient.joinToString(separator = ", ")
-        title_ingredient.setTitleValue("Ingredients : ", formatList(product.ingredient))
-
-        title_from.text = product.countryFrom.joinToString(separator = ", ")
-        title_from.setTitleValue("Vendu en : ", formatList(product.countryFrom))
-
-        title_allergen.text = product.allergen.joinToString(separator = ", ")
-        title_allergen.setTitleValue("Substances allergènes : ", formatList(product.allergen))
-
-        title_addictifs.text = product.addictif.joinToString(separator = ", ")
-        title_addictifs.setTitleValue("Addictifs : ", formatList(product.addictif))
-
-
-        val nutriscore = "B"
-
-        nutriscore_img.setImageResource(
-            resources.getIdentifier(
-                "nutriscore${nutriscore.toLowerCase()}",
-                "drawable",
-                packageName
-            )
-        )
-        Picasso.get().load(product.imgUrl).into(url_image)
-    }
-
-    fun formatList(list: List<String>): String {
-        return if (list.isEmpty()) {
-            getString(R.string.empty_ingredient)
-        } else {
-            list.joinToString(separator = ",")
-        }
-    }
-
-
-}
-
-fun TextView.setTitleValue(title: String, value: String) {
-    text = SpannableString("$title $value").apply {
-        setSpan(StyleSpan(Typeface.BOLD), 0, title.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
-    }
 }
 
 
@@ -198,13 +121,6 @@ data class Product(
 ) : Parcelable {
 
 }
-
-
-//open class Fragment : Fragment(){
-//    override fun onCreateView(name: String?, context: Context?, attrs: AttributeSet?): View? {
-//        return super.onCreateView(name, context, attrs)
-//    }
-//}
 
 
 
